11e7b1672be72578ae57561d988bf26489ec3cf1
mykolamayorov <mykolamayorov@gmail.com>
2025-11-13 20:35:13 +0200

    submit

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,61 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person came into the house");
+    } else {
+      console.log("The door is closed. Can't come in");
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open");
+    } else {
+      console.log("Wrong key");
+    }
+  }
+}
+
+// --- Тестування сценарію ---
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+house.openDoor(person.getKey()); // Відчиняє двері
+house.comeIn(person); // Людина заходить
 
 export {};
